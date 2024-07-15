74632098736363a6baa6382e08c11424de8b47ec
blessonnag <technicalguruji03011996@gmail.com>
2024-07-15 18:10:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title("😷Welcome To SkyFish")
+
+import pandas as pd
+
+video_url = 'https://youtu.be/cBYLKtKYUAY?si=FsYBnj5SIs-neI8o'
+# Display the video
+st.video(video_url)
+st.title("Justin bieber Favorite Song")
+video_url = 'https://youtu.be/2u2Z07ujyD8?si=gvSDs1z4nj9nZeRP'
+# Display the video
+st.video(video_url)
+st.title("Annie Marie 2002")
+video_url = 'https://youtu.be/Il-an3K9pjg?si=gE4YHj7GFBMas4sh'
+# Display the video
+st.video(video_url)
+
+
+
+
+
+
+
+
+
